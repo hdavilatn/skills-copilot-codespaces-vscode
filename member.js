59455f5ca75648e77210632e6c60df67fec906a5
Homar Dávila Padilla <hdavilap01@gmail.com>
2024-02-05 21:57:00 -0500

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMenbers() {
+  return [
+    {
+      name: 'Rafael',
+      skills: ['Node.js', 'React', 'TypeScript', 'Docker', 'Kubernetes'],
+    },
+    {
+      name: 'Rafael',
+      skills: ['Node.js', 'React', 'TypeScript', 'Docker', 'Kubernetes'],
+    },
+    {
+      name: 'Rafael',
+      skills: ['Node.js', 'React', 'TypeScript', 'Docker', 'Kubernetes'],
+    },
+  ];
+}
