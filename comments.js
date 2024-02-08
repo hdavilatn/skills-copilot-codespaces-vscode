ae91158813b4c554a41a611fbaff583a1631547d
Homar Dávila Padilla <hdavilap01@gmail.com>
2024-02-07 23:01:56 -0500

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,40 +1,16 @@
-//Create a web server
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var path = require('path');
+//Create a simple web server in node.js that serves the comments.js file to the browser. 
+//You can use the http module's createServer method to do this. 
+//The server should listen on port 4000.
 
-//create server
-http.createServer(function(req, res){
-    var pathname = url.parse(req.url).pathname;
-    var ext = path.extname(pathname);
-    var type = '';
-    switch(ext){
-        case '.html':
-            type = 'text/html';
-            break;
-        case '.js':
-            type = 'text/javascript';
-            break;
-        case '.css':
-            type = 'text/css';
-            break;
-        case '.jpg':
-            type = 'image/jpeg';
-            break;
-        case '.png':
-            type = 'image/png';
-            break;
-    }
-    if(type){
-        fs.readFile(__dirname + pathname, function(err, data){
-            if(err){
-                res.writeHead(500);
-                return res.end('Error loading ' + pathname);
-            }
-            res.writeHead(200, {'Content-Type': type});
-            res.end(data);
-        })
-    }
-}).listen(3000);
-console.log('Server running at http://localhost:3000/');
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/javascript'});
+    fs.createReadStream('comments.js').pipe(res);
+});
+
+server.listen(4000);
+console.log('Server is listening on port 4000');
+// The server should be able to serve the comments.js file to the browser.
+// The server should listen on port 4000.
