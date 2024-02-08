04ce6f9c616d5a5f2bc243118e0c3995fe0fb278
Homar Dávila Padilla <hdavilap01@gmail.com>
2024-02-07 23:10:33 -0500

    Copilot third commit

--- comments.js
+++ comments.js
@@ -5,10 +5,3 @@ function sum(a, b) {
 
 //Export the function
 module.exports = sum;
-
-// Path: index.js
-//Require the file
-var sum = require('./comments.js');
-//Call the function
-console.log(sum(1, 2)); //3
-
