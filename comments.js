45c22c785f5f90d1df5e35d549dbce794ff58583
Homar Dávila Padilla <hdavilap01@gmail.com>
2024-02-07 23:09:06 -0500

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,14 @@
-//Create a simple web server in node.js that serves the comments.js file to the browser. 
-//You can use the http module's createServer method to do this. 
-//The server should listen on port 4000.
+//Function to sum thow numbers
+function sum(a, b) {
+    return a + b;
+}
 
-var http = require('http');
-var fs = require('fs');
+//Export the function
+module.exports = sum;
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/javascript'});
-    fs.createReadStream('comments.js').pipe(res);
-});
+// Path: index.js
+//Require the file
+var sum = require('./comments.js');
+//Call the function
+console.log(sum(1, 2)); //3
 
-server.listen(4000);
-console.log('Server is listening on port 4000');
-// The server should be able to serve the comments.js file to the browser.
-// The server should listen on port 4000.
